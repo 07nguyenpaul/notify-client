eb99f54848f2530add939d840784ba1c4315f702
Ethan Bennett <ebennett19@gmail.com>
2018-02-17 12:49:46 -0700

    Add mocks

---
+++ src/assets/mocks/mockNotifications.js
@@ -0,0 +1,48 @@
+export const mockNotifications = [
+  {
+    address: '0xa7c01459e152881e3d70aa4106b2d006415690dd',
+    abi: [
+      {
+        constant: false,
+        inputs: [
+          {
+            name: '_message',
+            type: 'bytes32',
+          },
+        ],
+        name: 'hack',
+        outputs: [],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        anonymous: false,
+        inputs: [
+          {
+            indexed: false,
+            name: '_from',
+            type: 'address',
+          },
+          {
+            indexed: false,
+            name: '_message',
+            type: 'bytes32',
+          },
+        ],
+        name: 'LogHack',
+        type: 'event',
+      },
+    ],
+    phoneNumber: '9135159867',
+    type: 'contract',
+  },
+  {
+    address: '0x3Ba431a29E4093Ab34E44aEAbb6aFc62747B0444',
+    abi: [],
+    phoneNumber: '9135159867',
+    type: 'account',
+  },
+];
+
+export default mockNotifications;
