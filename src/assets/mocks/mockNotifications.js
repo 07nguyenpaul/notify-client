87bf141b125fd7323f90b245aa1f8daca3bba39a
Ethan Bennett <ebennett19@gmail.com>
2018-02-17 13:55:53 -0700

    Set up services

--- src/assets/mocks/mockNotifications.js
+++ src/assets/mocks/mockNotifications.js
@@ -36,12 +36,14 @@ export const mockNotifications = [
     ],
     phoneNumber: '9135159867',
     type: 'contract',
+    nonce: 0,
   },
   {
     address: '0x3Ba431a29E4093Ab34E44aEAbb6aFc62747B0444',
     abi: [],
     phoneNumber: '9135159867',
     type: 'account',
+    nonce: 0,
   },
 ];
 
