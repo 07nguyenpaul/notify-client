4d3b1e473821199b5a9f9cbc82aefffc261b3bdb
Ethan Bennett <ebennett19@gmail.com>
2018-02-18 01:43:05 -0700

    Remove metamask check

--- src/index.js
+++ src/index.js
@@ -14,18 +14,14 @@ const history = createBrowserHistory();
 const store = configureStore(history);
 
 const render = Component => {
-  if (window.web3) {
-    ReactDOM.render(
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Component />
-        </ConnectedRouter>
-      </Provider>,
-      document.getElementById('root')
-    );
-  } else {
-    console.log('Please download MetaMask.');
-  }
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Component />
+      </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+  );
 };
 
 render(App);
